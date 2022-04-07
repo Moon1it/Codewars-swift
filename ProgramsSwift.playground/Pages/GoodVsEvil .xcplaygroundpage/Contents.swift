0f88105d6b5a4d4e355928165ff5eed8da769650
Moon1it <viza0010@gmail.com>
2022-04-07 15:31:17 +0700

    update GoodVsEvil

--- ProgramsSwift.playground/Pages/GoodVsEvil .xcplaygroundpage/Contents.swift
+++ ProgramsSwift.playground/Pages/GoodVsEvil .xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-func Score(dict : [Int : Int], troops : String) -> Int{
+func Score(_ dict: [Int : Int],_ troops: String) -> Int{
     let troopsMass = troops.split(whereSeparator: {$0 == " "})
     var sumScore = 0
     for (i, el) in troopsMass.enumerated(){
@@ -6,20 +6,27 @@ func Score(dict : [Int : Int], troops : String) -> Int{
     }
     return sumScore
 }
-
 func evaluate(good: String, vsEvil: String) -> String {
     let goodDict = [0 : 1, 1 : 2, 2 : 3, 3 : 3, 4 : 4, 5 : 10]
     let evilDict = [0 : 1, 1 : 2, 2 : 2, 3 : 2, 4 : 3, 5 : 5, 6 : 10]
-    let goodScore = Score(dict: goodDict, troops: good)
-    let evilScore = Score(dict: evilDict, troops: vsEvil)
-    
-    if goodScore > evilScore{
+    switch Score(goodDict, good) - Score(evilDict, vsEvil){
+    case 1...:
         return "Battle Result: Good triumphs over Evil"
-    }else if goodScore < evilScore{
+    case ..<0:
         return "Battle Result: Evil eradicates all trace of Good"
-    }else{
+    default:
         return "Battle Result: No victor on this battle field"
     }
 }
 
 evaluate(good: "0 0 0 0 0 10", vsEvil: "0 1 1 1 1 0 0")
+
+// Используя замыкание
+//evaluate(good: "0 0 0 0 0 10", vsEvil: "0 1 1 1 1 0 0"){
+//    let troopsMass = $1.split(whereSeparator: {$0 == " "})
+//    var sumScore = 0
+//    for (i, el) in troopsMass.enumerated(){
+//        sumScore += ($0[i])! * Int(el)!
+//    }
+//    return sumScore
+//}
